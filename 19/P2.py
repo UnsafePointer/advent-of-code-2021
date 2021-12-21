2ec8f169e205e46ee0360c0f34f680392ebfe53b
Renzo Crisostomo <renzo@crisostomo.me>
2021-12-21 17:53:09 +0100

    Add Dec 19 solutions

---
+++ 19/P2.py
@@ -0,0 +1,236 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from math import sqrt
+from typing import List, Optional, Tuple, Set, Dict
+from itertools import combinations, permutations
+from collections import defaultdict
+from heapq import heappush, heappop
+from copy import deepcopy
+from sys import maxsize
+
+
+@dataclass
+class Beacon:
+    x: int
+    y: int
+    z: int
+    scanner: int
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y, self.z, self.scanner))
+
+    def distance(self, another: Beacon) -> float:
+        return round(
+            sqrt(
+                pow(another.x - self.x, 2)
+                + pow(another.y - self.y, 2)
+                + pow(another.z - self.z, 2)
+            ),
+            10,
+        )
+
+    def dimensions(self) -> List[int]:
+        return [self.x, self.y, self.z]
+
+
+@dataclass
+class Scanner:
+    identifier: int
+    beacons: Set[Beacon] = field(default_factory=set)
+
+    def distances(self) -> Dict[float, Tuple[Beacon, Beacon]]:
+        distances: Dict[float, Tuple[Beacon, Beacon]] = {}
+        for (a, b) in combinations(self.beacons, 2):
+            distance = a.distance(b)
+            distances[distance] = (a, b)
+        return distances
+
+    def common_beacons(self, another: Scanner) -> List[Tuple[Beacon, Beacon]]:
+        d0 = self.distances()
+        d1 = another.distances()
+        r = set(d0.keys()) & set(d1.keys())
+        beacons: Dict[Beacon, Set[Beacon]] = {}
+        for distance in r:
+            (a, b) = d0[distance]
+            if a in beacons:
+                beacons[a] &= set(d1[distance])
+            else:
+                beacons[a] = set(d1[distance])
+
+            if b in beacons:
+                beacons[b] &= set(d1[distance])
+            else:
+                beacons[b] = set(d1[distance])
+        beacon_mapping: List[Tuple[Beacon, Beacon]] = []
+        for k, v in beacons.items():
+            if len(v) == 1:
+                beacon_mapping.append((k, v.pop()))
+        return beacon_mapping
+
+    def add_beacons(
+        self,
+        another: Scanner,
+        signs: Tuple[int, int, int],
+        ref: Tuple[int, int, int],
+        indices: Tuple[int, int, int],
+    ) -> None:
+        (x_sign, y_sign, z_sign) = signs
+        (x_ref, y_ref, z_ref) = ref
+        (i, j, k) = indices
+        for beacon in another.beacons:
+            dims = beacon.dimensions()
+            x = (dims[i] * (x_sign * -1)) + x_ref
+            y = (dims[j] * (y_sign * -1)) + y_ref
+            z = (dims[k] * (z_sign * -1)) + z_ref
+            new_beacon = Beacon(x, y, z, self.identifier)
+            self.beacons.add(new_beacon)
+        return
+
+    def transform_position(
+        self,
+        position: Tuple[int, int, int],
+        signs: Tuple[int, int, int],
+        ref: Tuple[int, int, int],
+        indices: Tuple[int, int, int],
+    ) -> Tuple[int, int, int]:
+        (x, y, z) = position
+        (x_sign, y_sign, z_sign) = signs
+        (x_ref, y_ref, z_ref) = ref
+        (i, j, k) = indices
+        dims = [x, y, z]
+        x = (dims[i] * (x_sign * -1)) + x_ref
+        y = (dims[j] * (y_sign * -1)) + y_ref
+        z = (dims[k] * (z_sign * -1)) + z_ref
+        return (x, y, z)
+
+
+def find_position_relative_to_common_beacons(
+    beacon_mapping: List[Tuple[Beacon, Beacon]]
+) -> Optional[Tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]]]:
+    previous_ref: Optional[Tuple[int, int, int]]
+    for (i, j, k) in permutations(range(3)):
+        for x_sign in [-1, 1]:
+            for y_sign in [-1, 1]:
+                for z_sign in [-1, 1]:
+                    found = True
+                    previous_ref = None
+                    for (point_0, point_1) in beacon_mapping:
+                        dims = point_1.dimensions()
+                        ref = (
+                            point_0.x + (dims[i] * x_sign),
+                            point_0.y + (dims[j] * y_sign),
+                            point_0.z + (dims[k] * z_sign),
+                        )
+                        if previous_ref == None:
+                            previous_ref = ref
+                        else:
+                            if previous_ref != ref:
+                                found = False
+                                break
+                    if found:
+                        return ((x_sign, y_sign, z_sign), previous_ref, (i, j, k))
+    return None
+
+
+def get_path_to_zero(
+    current: int,
+    current_path: List[int],
+    known_paths: List[List[int]],
+    mapping: Dict[int, Set[int]],
+) -> Optional[List[int]]:
+    if current == 0 and current_path not in known_paths:
+        return current_path
+    for option in mapping[current]:
+        if option in current_path:
+            continue
+        option_path = current_path.copy()
+        option_path.append(option)
+        result = get_path_to_zero(option, option_path, known_paths, mapping)
+        if result != None:
+            return result
+    return None
+
+
+def manhattan_distance(
+    position_a: Tuple[int, int, int], position_b: Tuple[int, int, int]
+) -> int:
+    (a_x, a_y, a_z) = position_a
+    (b_x, b_y, b_z) = position_b
+    return abs(b_x - a_x) + abs(b_y - a_y) + abs(b_z - a_z)
+
+
+def solve() -> int:
+    input = open("input.txt", "r")
+
+    scanners: List[Scanner] = []
+    current_scanner: Scanner
+    for line in [s.strip() for s in input.readlines()]:
+        if line.startswith("---"):
+            identifier = int(line.split()[2])
+            current_scanner = Scanner(identifier=identifier)
+            scanners.append(current_scanner)
+        elif line == "":
+            continue
+        else:
+            (x, y, z) = tuple([int(s) for s in line.split(",")])
+            beacon = Beacon(x, y, z, current_scanner.identifier)
+            current_scanner.beacons.add(beacon)
+
+    scanner_mapping: Dict[int, Set[int]] = defaultdict(lambda: set())
+    for (a, b) in combinations(scanners, 2):
+        beacon_mapping = a.common_beacons(b)
+        if len(beacon_mapping) >= 12:
+            scanner_mapping[a.identifier].add(b.identifier)
+            scanner_mapping[b.identifier].add(a.identifier)
+
+    priority_queue: List[Tuple[int, List[int]]] = []
+    for idx in range(len(scanners)):
+        known_paths = []
+        while True:
+            path = get_path_to_zero(idx, [idx], known_paths, scanner_mapping)
+            if path:
+                known_paths.append(path)
+            else:
+                break
+        for known_path in known_paths:
+            heappush(priority_queue, (len(known_path), (idx, known_path)))
+
+    solved_count: Set[int] = set()
+    scanner_positions: Set[Tuple[int, int, int]] = set([(0, 0, 0)])
+    while priority_queue:
+        (_, (idx, path)) = heappop(priority_queue)
+        if idx in solved_count:
+            continue
+        if len(path) == 1:
+            continue
+        scanners_copy = deepcopy(scanners)
+        scanner_position: Tuple[int, int, int] = (0, 0, 0)
+        for i, j in zip(path, path[1:]):
+            b = scanners_copy[i]
+            a = scanners_copy[j]
+            beacon_mapping = a.common_beacons(b)
+            positions = find_position_relative_to_common_beacons(beacon_mapping)
+            (signs, beacon_relative, indices) = positions
+            a.add_beacons(b, signs, beacon_relative, indices)
+            scanner_position = a.transform_position(
+                scanner_position, signs, beacon_relative, indices
+            )
+        scanner_positions.add(scanner_position)
+        solved_count.add(idx)
+
+    max_manhattan_distance = -maxsize
+    for (position_a, position_b) in combinations(scanner_positions, 2):
+        max_manhattan_distance = max(
+            manhattan_distance(position_a, position_b), max_manhattan_distance
+        )
+
+    return max_manhattan_distance
+
+
+def main() -> None:
+    print(solve())
+
+
+if __name__ == "__main__":
+    main()
